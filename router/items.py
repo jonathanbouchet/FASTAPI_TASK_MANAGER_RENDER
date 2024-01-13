32ec2054f3fbf54efa498e18f7efaaf49eddc63e
Jonathan Bouchet <jonathanbouchet@Jonathans-MacBook-Pro.local>
2024-01-13 13:58:08 -0800

    added x-api-key security

--- router/items.py
+++ router/items.py
@@ -1,4 +1,6 @@
 from fastapi import APIRouter, HTTPException, Request, Depends, status
+from fastapi.security import APIKeyHeader
+from dotenv import dotenv_values
 from sqlalchemy.orm import Session
 from db.core import get_db, NotFoundError
 from db.items import (
@@ -13,12 +15,37 @@ from db.items import (
 )
 from .limit import limiter
 
+# get ax-api-key from environment
+app_config = dotenv_values(".env")
+X_API_Key = app_config["X-Api-Key"]
 router = APIRouter(prefix="/items", tags=["items"])
+
+oauth2_scheme = APIKeyHeader(name="X-API-Key") 
+
+def api_key_auth(api_key: str = Depends(oauth2_scheme)):
+    if api_key != X_API_Key:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Forbidden")
+    return api_key
 
 
 @router.get("/")
 @limiter.limit("10/minute")
 async def read_all_items(request: Request, db: Session = Depends(get_db)) -> list[Item]:
+    try:
+        db_items = read_db_items(db)
+    except NotFoundError as e:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND) from e
+    return [Item(**db_item.__dict__) for db_item in db_items]
+
+
+@router.get("/private")
+@limiter.limit("20/minute")
+async def read_all_items(request: Request, 
+                         db: Session = Depends(get_db), 
+                         api_key_depency=Depends(api_key_auth)) -> list[Item]:
+    print(f"Welcome to the private route, your x-api-key :{api_key_depency}")
     try:
         db_items = read_db_items(db)
     except NotFoundError as e:
